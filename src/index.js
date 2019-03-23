c4b681b1a78825dfc969dfb05dbcedebb3170251
Nastya Tsimanovich <nastya.cimanovich.97@gmail.com>
2019-03-23 16:56:18 +0300

    Add src.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let factors=[];
+  let i=2;
+  let n=base;
+  let p=0;
+  while (i*i<=n){
+      if(n%i==0){
+          factors.push(i);
+          n=Math.floor(n/i);
+      }
+      else{
+          i+=1;
+      }
+  }
+  factors.push(n);
+  
+  let count=0;
+      let j=factors[factors.length-1];
+      while(number>=j){
+      count+=Math.floor(number/j);
+      j*=factors[factors.length-1];
+    }
+    for (let k=0;k<=factors.length;k++){
+        if(factors[k]===factors[factors.length-1]){
+            p++;      
+        }
+    }
+    return Math.floor(count/p);
 }
